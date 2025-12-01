13d9ae35434548a2dfcc34423623468bcae89427
Himanshu-kk <hk4375417@gmail.com>
2025-12-01 18:53:07 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 // src/config.js
 // export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://movie-app-backend-drab.vercel.app";
+export const BASE_URL = "https://movie-app-backendhim.vercel.app";
 
 
